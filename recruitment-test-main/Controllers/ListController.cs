06c27ebde39ace08d129ec58e6c89fec2616d3d1
James Cairns <james.cairns@outlook.com>
2022-01-22 17:31:58 +0000

    CRUD functionality for ListController
    I believe I added CRUD functionality to the ListController, however, I have not done CRUD this way before and will have to check this works.
    
    TODO:
    SQL stuff
    - Increment the field Value by 1 where the field Name begins with ‘E’ and by 10  where Name begins with ‘G’ and all others by 100
    - List the sum of all Values for all Names that begin with A, B or C but only present the data where the summed values are greater than or equal to 11171
    
    View stuff
    - Make components to display a list of employees, add, edit, delete

--- recruitment-test-main/Controllers/ListController.cs
+++ recruitment-test-main/Controllers/ListController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InterviewTest.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
 
 namespace InterviewTest.Controllers
 {
@@ -13,5 +16,87 @@ namespace InterviewTest.Controllers
         /*
          * List API methods goe here
          * */
+
+        [HttpGet] //READ
+        public JsonResult GetEmployeeList()
+        {
+            var employees = new List<Employee>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"SELECT Name, Value FROM Employees";
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        employees.Add(new Employee
+                        {
+                            Name = reader.GetString(0),
+                            Value = reader.GetInt32(1)
+                        });
+                    }
+                }
+            }
+
+            return new JsonResult(employees);
+        }
+
+        [HttpPut] //UPDATE
+        public JsonResult PutEmployee(Employee employee)
+        {
+            var employees = new List<Employee>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"UPDATE Employees SET Value = " + employee.Value + @" WHERE Name = '" + employee.Name + @"'";
+            }
+
+            return new JsonResult(employees);
+        }
+
+
+        [HttpPost] //CREATE
+        public JsonResult PostEmployee(Employee employee)
+        {
+            var employees = new List<Employee>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"INSERT INTO Employees VALUES ('" + employee.Name + @"', " + employee.Value + @")";
+                queryCmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult(employees);
+        }
+
+        [HttpDelete("{Name}")] //DELETE
+        public JsonResult DeleteEmployee(string Name)
+        {
+            var employees = new List<Employee>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"DELETE FROM Employees WHERE Name = '" + Name + @"'";
+                queryCmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult(employees);
+        }
     }
 }
